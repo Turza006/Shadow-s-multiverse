137cf56bdcc79bed53ac10a8386861df7aec2ab5
Turza006 <probaldattaturza@gmail.com>
2020-12-23 01:20:33 +0600

    Beta 1

---
+++ index.js
@@ -0,0 +1,40 @@
+function getGrid1(index, imageLink, imageName) {
+    return `<div class="row">
+    <div class="bx bx-1">
+        <div class="card"> <img class="card-img" src="assets/img/portfolio-img1.png" alt="">
+            <div class="card-img-overlay"> <a href="#"><img src="assets/img/heart.png" class="heart" alt="heart icon"></a>
+                <div class="bottom-text">
+                    <h5 class="card-title">Natural site</h5>
+                </div>
+            </div>
+        </div>
+    </div>`
+}
+
+function getGrid2(index, imageLink, imageName) {
+    return ``
+}
+
+function getGrid3(index, imageLink, imageName) {
+    return ``
+}
+function getGrid4(index, imageLink, imageName) {
+    return ``
+}
+function getGrid5(index, imageLink, imageName) {
+    return ``
+}
+function getGrid6(index, imageLink, imageName) {
+    return ``
+}
+function getGrid7 (index, imageLink, imageName) {
+    return ``
+}
+function getGrid1(index, imageLink, imageName) {
+    return ``
+}
+function getGrid1(index, imageLink, imageName) {
+    return ``
+}
+
+
